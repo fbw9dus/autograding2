2c079fee09fdac0cffca047ff2cba20558892217
galymax <werkhowski@gmail.com>
2020-03-22 22:22:13 +0100

    test files

---
+++ __tests__/get.test.js
@@ -0,0 +1,26 @@
+const app = require('../app')
+const request = require('supertest')
+const mongoose = require('mongoose')
+let server;
+
+describe('App Request', () => {
+  test('should have db entry', async (done) => {
+    const response = await request(app)
+    .get('/records')
+    expect(response.statusCode).toEqual(200)
+    expect(response.body[0].artist).toBe("George Michael")
+    done();
+  })
+})
+
+beforeAll(async (done) => {
+  server = app.listen(3000, () => {
+    global.agent = request.agent(server);
+    done();
+  });
+});
+
+afterAll(async () => {
+  await server.close();
+  await mongoose.disconnect();
+});

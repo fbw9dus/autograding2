6a3580cbe6cf6108d98c2e67a3520f3ea7080830
galymax <werkhowski@gmail.com>
2020-03-22 21:41:39 +0100

    autograding

--- hello.test.js
+++ hello.test.js
@@ -1,8 +1,26 @@
 const app = require('./app')
 const request = require('supertest')
+const mongoose = require('mongoose')
+let server;
 
-test('gets the test endpoint', async () => {
-  const response = await request(app).get('/records').expect(200)
-  expect(response.statusCode).toEqual(200)
-  expect(response.body[0].artist).toBe("George Michael")
+beforeAll(async (done) => {
+  server = app.listen(3000, () => {
+    global.agent = request.agent(server);
+    done();
+  });
+});
+
+describe('App Request', () => {
+  test('should responds with 404', async (done) => {
+    const response = await request(app)
+    .get('/records')
+    expect(response.statusCode).toEqual(200)
+    expect(response.body[0].artist).toBe("George Michael")
+    done();
+  })
 })
+
+afterAll(async () => {
+  await server.close();
+  await mongoose.disconnect();
+});
